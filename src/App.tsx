47650401646ddfc3871f36b4cb3d9193f61f5610
Charles Li <96878550+41CharlesLi@users.noreply.github.com>
2022-09-30 09:02:40 -0400

    set up routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Store } from "./pages/Store";
+import { About } from "./pages/About";
+import { Navbar } from "./components/Navbar";
 import { Container } from "react-bootstrap";
 
 function App() {
     return (
-        <Container>
-            <Routes></Routes>
+        <Container className="mb-4">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/store" element={<Store />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
         </Container>
     );
 }
